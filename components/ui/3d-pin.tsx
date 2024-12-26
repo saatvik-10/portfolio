154abe2c75ebeb4548a0dcf41a86e738daefed64
saatvik-10 <saatvikmadan529@gmail.com>
2024-12-26 20:51:18 +0530

    fixes in cards

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -50,7 +50,7 @@ export const PinContainer = ({
           style={{
             transform: transform,
           }}
-          className='absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-black border border-emerald-500 group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden'
+          className='absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-black border border-emerald-500 group-hover/pin:border-emerald-600 transition duration-700 overflow-hidden'
         >
           <div className={cn(' relative z-50 ', className)}>{children}</div>
         </div>
@@ -74,7 +74,7 @@ export const PinPerspective = ({
           <a
             href={href}
             target={'_blank'}
-            className='relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 '
+            className='relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-emerald-500 '
           >
             <span className='relative z-20 text-white text-xs font-bold inline-block py-0.5'>
               {title}
@@ -110,7 +110,7 @@ export const PinPerspective = ({
                 repeat: Infinity,
                 delay: 0,
               }}
-              className='absolute left-1/2 top-1/2  h-[11.25rem] w-[11.25rem] rounded-[50%] bg-sky-500/[0.08] shadow-[0_8px_16px_rgb(0_0_0/0.4)]'
+              className='absolute left-1/2 top-1/2  h-[11.25rem] w-[11.25rem] rounded-[50%] bg-emerald-500/[0.4] shadow-[0_8px_16px_rgb(0_0_0/0.4)]'
             ></motion.div>
             <motion.div
               initial={{
@@ -130,7 +130,7 @@ export const PinPerspective = ({
                 repeat: Infinity,
                 delay: 2,
               }}
-              className='absolute left-1/2 top-1/2  h-[11.25rem] w-[11.25rem] rounded-[50%] bg-sky-500/[0.08] shadow-[0_8px_16px_rgb(0_0_0/0.4)]'
+              className='absolute left-1/2 top-1/2  h-[11.25rem] w-[11.25rem] rounded-[50%] bg-emerald-500/[0.4] shadow-[0_8px_16px_rgb(0_0_0/0.4)]'
             ></motion.div>
             <motion.div
               initial={{
