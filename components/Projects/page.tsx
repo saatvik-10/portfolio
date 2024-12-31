0b7ea6f339347db5bb219bbcaf77fece65b639fc
saatvik-10 <saatvikmadan529@gmail.com>
2025-01-01 01:46:25 +0530

    applied prettier

--- components/Projects/page.tsx
+++ components/Projects/page.tsx
@@ -4,7 +4,9 @@ import { ProjectsList } from './ProjectsList';
 const ProjectsSection = () => {
   return (
     <main className='flex flex-col py-20'>
-      <span className='text-emerald-500 mb-16 text-7xl font-bold'>My Projects</span>
+      <span className='text-emerald-500 mb-16 text-7xl font-bold'>
+        My Projects
+      </span>
       <section className='grid grid-cols-1 md:grid-cols-3 gap-8'>
         {ProjectsList.map((project, index) => (
           <Projects
