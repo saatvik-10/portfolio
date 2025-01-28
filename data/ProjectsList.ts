95cd70b89b8ee4ddae4705727ace0bafaac953b9
Saatvik__10 <146160585+saatvik-10@users.noreply.github.com>
2025-01-28 21:43:57 +0530

    Updated ProjectsList

--- data/ProjectsList.ts
+++ data/ProjectsList.ts
@@ -1,17 +1,17 @@
 export const ProjectsList = [
   {
+    title: 'ElevateHQ',
+    href: 'https://elevatehq.itsaatvik.dev/',
+    name: 'GitHub SaaS',
+    desc: 'A R.A.G. website',
+    img: '/projects/elevatehq.png',
+  },
+  {
     title: 'Roomify',
     href: 'https://roomify.itsaatvik.dev/',
     name: 'Roomify',
     desc: 'A Room Booking application',
     img: '/projects/roomify.png',
-  },
-  {
-    title: 'Expense Tracker',
-    href: 'https://expense-tracker.itsaatvik.dev/',
-    name: 'Expense Tracker',
-    desc: 'An Expense Tracker',
-    img: '/projects/expense.png',
   },
   {
     title: 'Dashboard',
