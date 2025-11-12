5c4e8639fb99da13be2e5de6daa03cf02582f26e
Saatvik__10 <146160585+saatvik-10@users.noreply.github.com>
2025-11-12 13:33:13 +0530

    Add new project entries

--- data/ProjectsList.ts
+++ data/ProjectsList.ts
@@ -15,9 +15,30 @@ export const ProjectsList = [
   },
   {
     title: 'Visit Website',
+    href: 'https://fight-club.itsaatvik.dev/',
+    name: 'Fight Club',
+    desc: 'First rule of FIGHT CLUB is You DO NOT talk about FIGHT CLUB',
+    img: '/projects/fight-club.png',
+  },
+  {
+    title: 'Visit Website',
     href: 'https://roomify.itsaatvik.dev/',
     name: 'Roomify',
     desc: 'App built with Appwrite, simplifies booking and managing room reservations.',
     img: '/projects/roomify.png',
+  },
+  {
+    title: 'Visit Website',
+    href: 'https://vaultigo.itsaatvik.dev/',
+    name: 'Vaultigo',
+    desc: 'A modern and secure cloud-based file manager within a structured folder system.',
+    img: '/projects/vaultigo.png',
+  },
+  {
+    title: 'Visit Website',
+    href: 'https://admin-dashboard.itsaatvik.dev/',
+    name: 'Admin Dashboard',
+    desc: 'Dashboard with bold UI and proper monitoring',
+    img: '/projects/admin-dashboard.png',
   },
 ];
